bf4f6fcefb501bbeddf29d8c00b39f2c31924dfb
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-04-19 23:31:49 -0600

    Add resposnive scale

--- src/components/BasicItem.jsx
+++ src/components/BasicItem.jsx
@@ -1,18 +1,34 @@
 import React, { useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 
 const BasicItem = (props) => {
   const meshRef = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
+  /*
 
   useFrame((state, delta) => (meshRef.current.rotation.y += delta));
+  useThree(
+    (width, height) =>
+      (meshRef.current.scale = [width * 0.25, width * 0.25, width * 0.25])
+  );
+  */
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += delta;
+      meshRef.current.scale.set(
+        state.size.width * 0.001,
+        state.size.width * 0.001,
+        state.size.width * 0.001
+      );
+    }
+  });
 
   return (
     <mesh
       {...props}
       ref={meshRef}
-      scale={active ? 1.5 : 1}
+      //scale={active ? 1.5 : 1}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
