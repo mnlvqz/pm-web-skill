220d4274d6ca4e3f014b61929e61bebdb0c0c829
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2024-06-19 02:33:38 -0600

    Update structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { Scroll, ScrollControls } from "@react-three/drei";
 import Overlay from "./components/Overlay";
+import Elements from "./components/Elements";
 
 const App = () => {
   //const [backgroundColor, setBackgroundColor] = useState("orange");
   // orthographic
   // camera={{ fov: 45.0, zoom: 1, position: [0, 0, 50] }
+  //<color attach="background" args={["#000000"]} />
   return (
-    <Canvas>
+    <Canvas orthographic camera={{ zoom: 200, position: [0, 0, 100] }}>
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={0.5}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <ambientLight intensity={Math.PI * 0.5} />
       <ScrollControls horizontal={false} pages={10} damping={0.25}>
-        <Scroll></Scroll>
+        <Scroll>
+          <Elements />
+        </Scroll>
         <Scroll html style={{ width: "100%" }}>
           <Overlay />
         </Scroll>
